cd6a46587293a1eeb44997b5ff3c838e1bfa769d
Kudirat Ijeoma Ibeabuchi <52330236+Qudrot@users.noreply.github.com>
2020-08-06 13:10:05 +0100

    create a method that output the fibonacci series

--- FibonacciSeries.java
+++ FibonacciSeries.java
@@ -1,14 +1,15 @@
 public class FibonacciSeries {
-    public static void main(String[] args) {
-        int n = 10;
-        int t1 = 0;
-        int t2 = 1;
+    public static void fibonacciSeries(int n, int t1, int t2) {
         System.out.print("First " + n + " terms: ");
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < n; i++) {
             System.out.print(t1 + " + ");
             int sum = t2 + t1;
             t1 = t2;
             t2 = sum;
         }
     }
+    public static void main(String[] args) {
+        fibonacciSeries(20,0,1);
+    }
 }
+
